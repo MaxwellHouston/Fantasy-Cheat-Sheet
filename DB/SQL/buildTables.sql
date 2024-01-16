fb7e0c5af22249615d30d7b672f9ddb9cff6302b
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2024-01-16 15:30:48 -0500

    Added player_stats table to SQL files

--- DB/SQL/buildTables.sql
+++ DB/SQL/buildTables.sql
@@ -121,3 +121,136 @@ ON players.id = fantasy_weeks.player_id
 WHERE pos = 'TE'
 GROUP BY team_id
 )sub;
+/* Player Final Stats */
+create table player_stats as (
+  SELECT *
+    FROM(
+      SELECT 
+      name, 
+      id, 
+      fantasy_weeks.team_id,
+      pos, 
+      CAST(ROW_NUMBER() OVER (ORDER BY SUM(ppr) DESC) AS int) AS Rank, 
+      CAST(sum(PPR) AS int) AS total, 
+      CAST(ROUND(AVG(ppr), 2) AS int) AS ptsG,
+      CAST(SUM(passing_weeks.att) AS int) AS passing_Att,
+      CAST(SUM(passing_weeks.cmp) AS int) AS passing_Cmp,
+      CAST(SUM(passing_weeks.yds) AS int) AS passing_Yards,
+      CAST(SUM(passing_weeks.td) AS int) AS passing_Tds,
+      CAST(SUM(passing_weeks.int) AS int) AS passing_Int,
+      CAST(SUM(rushing_weeks.att) AS int) AS rushing_Att,
+      CAST(SUM(rushing_weeks.yds) AS int) AS rushing_Yards,
+      CAST(SUM(rushing_weeks.td) AS int) AS rushing_Tds,
+      CAST(SUM(receiving_weeks.tgt) AS int) AS receiving_Tgt,
+      CAST(SUM(receiving_weeks.rec) AS int) AS receiving_Rec,
+      CAST(SUM(receiving_weeks.yds) AS int) AS receiving_Yards,
+      CAST(SUM(receiving_weeks.td) AS int) AS receiving_Tds
+      FROM players
+      JOIN fantasy_weeks
+      ON players.id = fantasy_weeks.player_id
+      LEFT JOIN passing_weeks
+      ON passing_weeks.player_id = players.id AND passing_weeks.week = fantasy_weeks.week
+      LEFT JOIN rushing_weeks
+      ON rushing_weeks.player_id = players.id AND rushing_weeks.week = fantasy_weeks.week
+      LEFT JOIN receiving_weeks
+      ON receiving_weeks.player_id = players.id AND receiving_weeks.week = fantasy_weeks.week
+      WHERE pos = 'QB'
+      GROUP BY name, id, fantasy_weeks.team_id, pos
+      UNION
+      SELECT 
+      name, 
+      id, 
+      fantasy_weeks.team_id,
+      pos, 
+      CAST(ROW_NUMBER() OVER (ORDER BY SUM(ppr) DESC) AS int) AS Rank, 
+      CAST(sum(PPR) AS int) AS total, 
+      CAST(ROUND(AVG(ppr), 2) AS int) AS ptsG,
+      CAST(SUM(passing_weeks.att) AS int) AS passing_Att,
+      CAST(SUM(passing_weeks.cmp) AS int) AS passing_Cmp,
+      CAST(SUM(passing_weeks.yds) AS int) AS passing_Yards,
+      CAST(SUM(passing_weeks.td) AS int) AS passing_Tds,
+      CAST(SUM(passing_weeks.int) AS int) AS passing_Int,
+      CAST(SUM(rushing_weeks.att) AS int) AS rushing_Att,
+      CAST(SUM(rushing_weeks.yds) AS int) AS rushing_Yards,
+      CAST(SUM(rushing_weeks.td) AS int) AS rushing_Tds,
+      CAST(SUM(receiving_weeks.tgt) AS int) AS receiving_Tgt,
+      CAST(SUM(receiving_weeks.rec) AS int) AS receiving_Rec,
+      CAST(SUM(receiving_weeks.yds) AS int) AS receiving_Yards,
+      CAST(SUM(receiving_weeks.td) AS int) AS receiving_Tds
+      FROM players
+      JOIN fantasy_weeks
+      ON players.id = fantasy_weeks.player_id
+      LEFT JOIN passing_weeks
+      ON passing_weeks.player_id = players.id AND passing_weeks.week = fantasy_weeks.week
+      LEFT JOIN rushing_weeks
+      ON rushing_weeks.player_id = players.id AND rushing_weeks.week = fantasy_weeks.week
+      LEFT JOIN receiving_weeks
+      ON receiving_weeks.player_id = players.id AND receiving_weeks.week = fantasy_weeks.week
+      WHERE pos = 'RB'
+      GROUP BY name, id, fantasy_weeks.team_id, pos
+      UNION
+      SELECT 
+      name, 
+      id, 
+      fantasy_weeks.team_id,
+      pos, 
+      CAST(ROW_NUMBER() OVER (ORDER BY SUM(ppr) DESC) AS int) AS Rank, 
+      CAST(sum(PPR) AS int) AS total, 
+      CAST(ROUND(AVG(ppr), 2) AS int) AS ptsG,
+      CAST(SUM(passing_weeks.att) AS int) AS passing_Att,
+      CAST(SUM(passing_weeks.cmp) AS int) AS passing_Cmp,
+      CAST(SUM(passing_weeks.yds) AS int) AS passing_Yards,
+      CAST(SUM(passing_weeks.td) AS int) AS passing_Tds,
+      CAST(SUM(passing_weeks.int) AS int) AS passing_Int,
+      CAST(SUM(rushing_weeks.att) AS int) AS rushing_Att,
+      CAST(SUM(rushing_weeks.yds) AS int) AS rushing_Yards,
+      CAST(SUM(rushing_weeks.td) AS int) AS rushing_Tds,
+      CAST(SUM(receiving_weeks.tgt) AS int) AS receiving_Tgt,
+      CAST(SUM(receiving_weeks.rec) AS int) AS receiving_Rec,
+      CAST(SUM(receiving_weeks.yds) AS int) AS receiving_Yards,
+      CAST(SUM(receiving_weeks.td) AS int) AS receiving_Tds
+      FROM players
+      JOIN fantasy_weeks
+      ON players.id = fantasy_weeks.player_id
+      LEFT JOIN passing_weeks
+      ON passing_weeks.player_id = players.id AND passing_weeks.week = fantasy_weeks.week
+      LEFT JOIN rushing_weeks
+      ON rushing_weeks.player_id = players.id AND rushing_weeks.week = fantasy_weeks.week
+      LEFT JOIN receiving_weeks
+      ON receiving_weeks.player_id = players.id AND receiving_weeks.week = fantasy_weeks.week
+      WHERE pos = 'WR'
+      GROUP BY name, id, fantasy_weeks.team_id, pos
+      UNION
+      SELECT 
+      name, 
+      id, 
+      fantasy_weeks.team_id,
+      pos, 
+      CAST(ROW_NUMBER() OVER (ORDER BY SUM(ppr) DESC) AS int) AS Rank, 
+      CAST(sum(PPR) AS int) AS total, 
+      CAST(ROUND(AVG(ppr), 2) AS int) AS ptsG,
+      CAST(SUM(passing_weeks.att) AS int) AS passing_Att,
+      CAST(SUM(passing_weeks.cmp) AS int) AS passing_Cmp,
+      CAST(SUM(passing_weeks.yds) AS int) AS passing_Yards,
+      CAST(SUM(passing_weeks.td) AS int) AS passing_Tds,
+      CAST(SUM(passing_weeks.int) AS int) AS passing_Int,
+      CAST(SUM(rushing_weeks.att) AS int) AS rushing_Att,
+      CAST(SUM(rushing_weeks.yds) AS int) AS rushing_Yards,
+      CAST(SUM(rushing_weeks.td) AS int) AS rushing_Tds,
+      CAST(SUM(receiving_weeks.tgt) AS int) AS receiving_Tgt,
+      CAST(SUM(receiving_weeks.rec) AS int) AS receiving_Rec,
+      CAST(SUM(receiving_weeks.yds) AS int) AS receiving_Yards,
+      CAST(SUM(receiving_weeks.td) AS int) AS receiving_Tds
+      FROM players
+      JOIN fantasy_weeks
+      ON players.id = fantasy_weeks.player_id
+      LEFT JOIN passing_weeks
+      ON passing_weeks.player_id = players.id AND passing_weeks.week = fantasy_weeks.week
+      LEFT JOIN rushing_weeks
+      ON rushing_weeks.player_id = players.id AND rushing_weeks.week = fantasy_weeks.week
+      LEFT JOIN receiving_weeks
+      ON receiving_weeks.player_id = players.id AND receiving_weeks.week = fantasy_weeks.week
+      WHERE pos = 'TE'
+      GROUP BY name, id, fantasy_weeks.team_id, pos
+    )a
+);
